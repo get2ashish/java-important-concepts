545e0b7372c6e54f950a34f08cadef337b16fa0e
Ashish Shukla <get2_ashish@yahoo.com>
2023-07-05 21:17:07 -0700

    LRU Cache

---
+++ src/main/java/ashish/examples/cache/LRUCache.java
@@ -0,0 +1,60 @@
+package ashish.examples.cache;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Objects;
+
+public class LRUCache {
+    private Map<Integer, String> cache;
+    private LinkedList<Integer> order;
+    private final int CACHE_SIZE;
+
+    LRUCache(int cacheSize) {
+        this.CACHE_SIZE = cacheSize;
+        order = new LinkedList<>();
+        cache = new HashMap<>();
+    }
+
+    void addToCache(int key, String value) {
+        if (order.size() >= CACHE_SIZE) {
+            int keyRemoved = order.removeLast();
+            cache.remove(keyRemoved);
+        }
+        order.addFirst(key);
+        cache.put(key, value);
+    }
+
+    String getFromCache(int key) {
+        String res = cache.get(key);
+        if (Objects.nonNull(res)) {
+            order.remove((Object) key);
+            order.addFirst(key);
+        }
+        return res;
+    }
+    public void display() {
+        for (int key : order) {
+            System.out.println(key + " => " + cache.get(key));
+        }
+    }
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(3);
+        cache.addToCache(1, "one");
+        cache.addToCache(2, "two");
+        cache.addToCache(3, "three");
+        cache.addToCache(4, "four");
+
+        System.out.println(cache.getFromCache(3));
+        System.out.println(cache.getFromCache(2));
+        System.out.println(cache.getFromCache(1));
+
+        cache.addToCache(1, "one");
+
+        System.out.println(cache.getFromCache(3));
+        System.out.println(cache.getFromCache(1));
+        cache.display();
+    }
+
+}
