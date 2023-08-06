6aea3eb873d9e5ea659c37e346efe51291fccc1c
Ashish Shukla <get2_ashish@yahoo.com>
2023-08-06 10:13:41 -0700

    Recursion factorial

--- src/main/java/ashish/examples/problems/FactorialRecursionExample.java
+++ src/main/java/ashish/examples/problems/FactorialRecursionExample.java
@@ -2,11 +2,15 @@ package ashish.examples.problems;
 
 public class FactorialRecursionExample {
     public static void main(String[] args) {
-        int factorial = factorial(6);
+        int factorial = factorial(5);
         System.out.println(factorial);
     }
 
     public static int factorial(int number) {
-        return number > 1 ? number * factorial(number - 1) : 1;
+        //we can also use ternary operator like return (number > 1) ? number * factorial(n-1) : 1;
+        if (number > 1) {
+            return number * factorial(number - 1);
+        }
+        return 1;
     }
 }
